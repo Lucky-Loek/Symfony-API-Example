cdf84fefbdc66113f9cf05cb9454fe71c465a191
Loek van der Linde <lind0077@hz.nl>
2017-01-17 13:18:58 +0100

    Update functional tests

--- src/AppBundle/Features/Context/FeatureContext.php
+++ src/AppBundle/Features/Context/FeatureContext.php
@@ -3,9 +3,15 @@
 namespace AppBundle\Features\Context;
 
 use AppBundle\Entity\Car;
+use AppBundle\Entity\User;
 use Behat\Behat\Context\Context;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Symfony2Extension\Context\KernelDictionary;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+use Sanpi\Behatch\Context\RestContext;
 
 class FeatureContext implements Context
 {
@@ -13,14 +19,43 @@ class FeatureContext implements Context
     use KernelDictionary;
 
     /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var array
+     */
+    protected $user;
+
+    /**
+     * @var RestContext
+     */
+    protected $restContext;
+
+    /**
+     * @param \Behat\Behat\Hook\Scope\BeforeScenarioScope $scope
+     *
      * @BeforeScenario
      */
-    public function clearDatabase()
+    public function gatherContexts(BeforeScenarioScope $scope)
+    {
+        $environment = $scope->getEnvironment();
+        $this->restContext = $environment->getContext('Sanpi\Behatch\Context\RestContext');
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function clearDatabaseAndTokenAndUser()
     {
         $manager = $this->getContainer()->get('doctrine')->getManager();
         $purger = new ORMPurger($manager);
         $purger->setPurgeMode(ORMPurger::PURGE_MODE_TRUNCATE);
         $purger->purge();
+
+        $this->token = '';
+        $this->user = null;
     }
 
     /**
@@ -33,5 +68,53 @@ class FeatureContext implements Context
         $em = $this->getContainer()->get('doctrine')->getManager();
         $em->persist($car);
         $em->flush();
+    }
+
+    /**
+     * @Given /^I am a user with username "([^"]*)" and password "([^"]*)"$/
+     */
+    public function iAmAUserWithUsernameAndPassword($username, $plainPassword)
+    {
+        $user = new User();
+
+        $encoder = $this->getContainer()->get('security.password_encoder');
+        $encodedPassword = $encoder->encodePassword($user, $plainPassword);
+
+        $user->setUsername($username);
+        $user->setPassword($encodedPassword);
+
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $this->user = [
+            'username' => $username,
+            'password' => $plainPassword,
+        ];
+    }
+
+    /**
+     * @Given I have a valid token
+     */
+    public function iHaveAValidToken()
+    {
+        $this->restContext->iAddHeaderEqualTo('username', $this->user['username']);
+        $this->restContext->iAddHeaderEqualTo('password', $this->user['password']);
+
+        $response = $this->restContext->iSendARequestTo('POST', $this->getContainer()->getParameter('app_url') . '/api/token');
+
+        $body = $response->getContent();
+
+        $body = json_decode($body);
+
+        $this->token = $body->token;
+    }
+
+    /**
+     * @Given /^I add that token to my request as Authorization header$/
+     */
+    public function iAddThatTokenToMyRequestAsAuthorizationHeader()
+    {
+        $this->restContext->iAddHeaderEqualTo('Authorization', 'Bearer '. $this->token);
     }
 }
